41b5620bf15f7e3faf0df6e2c313f1790117389f
Neil Dishington <neil_dishington@yahoo.co.uk>
2014-10-26 12:08:53 +0000

    correct typo

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,6 @@
 ## Matrix inversion is usually a costly computation and their may be some 
 ## benefit to caching the inverse of a matrix rather than compute it 
-## repeatedly. The functions do offer this functionality.
+## repeatedly. The functions here offer this functionality.
 
 ## This function creates a special "matrix" object that can cache its inverse.
 ## Arguments: a square invertible matrix
